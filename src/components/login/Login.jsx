7bdf9026e25b33e8d816dcb44595814908970021
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-12-05 21:08:49 +0000

    basic login/register added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,6 +4,17 @@ const Login = () => {
     return(
         <div>
             <h2>Login</h2>
+            <form>
+                <div>
+                    <label>Username</label>
+                    <input placeholder="Insert username here..." />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input placeholder="Insert password here" type="password" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -4,6 +4,21 @@ const Register = () => {
     return(
         <div>
             <h2>Register</h2>
+            <form>
+                <div>
+                    <label>Username</label>
+                    <input placeholder="Insert username here..." />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input placeholder="Insert email here..." type="email" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input placeholder="Insert password here" type="password" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
